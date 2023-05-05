3bba9eb5c609a2e5263e9a058f08f1d6b7574b31
hvaandres <hvaandres@icloud.com>
2023-05-05 16:12:10 -0600

    LaunchedEffect and rememberUpdateState

--- AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
+++ AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
@@ -24,13 +24,23 @@ import androidx.compose.samples.crane.R
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import kotlinx.coroutines.delay
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
 
 private const val SplashWaitTime: Long = 2000
 
 @Composable
 fun LandingScreen(onTimeout: () -> Unit, modifier: Modifier = Modifier) {
     Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+
+        val currenOnTimeout by rememberUpdatedState(onTimeout)
         // TODO Codelab: LaunchedEffect and rememberUpdatedState step
+        LaunchedEffect(onTimeout){
+            delay(SplashWaitTime)// This will simulate loading things
+            onTimeout()
+        }
         // TODO: Make LandingScreen disappear after loading data
         Image(painterResource(id = R.drawable.ic_crane_drawer), contentDescription = null)
     }
